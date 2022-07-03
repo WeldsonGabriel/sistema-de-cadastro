7aff8002c7a914df46cfa866dab113ab08c57777
W.Gabriel <gabrielborgesw2@gmail.com>
2022-07-02 21:59:45 -0300

    primeiro comite

---
+++ interface.py
@@ -0,0 +1,20 @@
+#criar interface
+from PySimpleGUI import PySimpleGUI as sg
+#layout (tema SystemDefault1).
+sg.theme('SystemDefault1')
+layout = [
+    [sg.text('usuario'),sg.Input(key='usuario')],
+    [sg.text('senha'),sg.Input(key='senha',password_char='*')],
+    [sg.Checkbox('salvar o login ?')],
+    [sg.Button('Entrar')]
+]
+#janela
+janela = sg.Window('tela de login', layout)
+#eventos
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar': 
+        if valores['usuario'] == 'Gabriel' and valores['senha'] == '123456':
+            print('Bem Vindo!')
